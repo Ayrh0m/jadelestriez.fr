55f95d9fa5d037c12e83349e99450303f02f0eff
Thomas Seyroles <seytom00@gmail.com>
2026-01-27 22:37:02 +0100

    style(home, footer): implement responsive design for home page and footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import SEO from "../components/SEO";
 import { usePageExitAnimation } from "../hooks/usePageExitAnimation";
 import HomeHero from "../components/pages/home/HomeHero";
 import HomeImage from "../components/pages/home/HomeImage";
+import Footer from "../components/Footer";
 
 export default function Home() {
   const { isExiting, handleExitComplete } = usePageExitAnimation();
@@ -24,20 +25,23 @@ export default function Home() {
   };
 
   return (
-    <motion.div
-      className={styles["home-container"]}
-      initial="hidden"
-      animate={isExiting ? "exit" : "visible"}
-      variants={pageVariants}
-      onAnimationComplete={handleExitComplete}
-    >
-      <SEO
-        title="Jade Lestriez"
-        description="Portfolio de Jade, étudiante en communication spécialisée dans le design et la création digitale. Découvrez mes projets et mon univers."
-      />
+    <>
+      <motion.div
+        className={styles["home-container"]}
+        initial="hidden"
+        animate={isExiting ? "exit" : "visible"}
+        variants={pageVariants}
+        onAnimationComplete={handleExitComplete}
+      >
+        <SEO
+          title="Jade Lestriez"
+          description="Portfolio de Jade, étudiante en communication spécialisée dans le design et la création digitale. Découvrez mes projets et mon univers."
+        />
 
-      <HomeHero />
-      <HomeImage />
-    </motion.div>
+        <HomeHero />
+        <HomeImage />
+      </motion.div>
+      <Footer className={styles["footer-home"]} />
+    </>
   );
 }
